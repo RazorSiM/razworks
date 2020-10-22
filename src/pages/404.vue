c56ba3b9630243cb451477d57b63198257574035
Simone Colabufalo <simone.colabufalo@gmail.com>
2020-10-23 01:37:24 +0200

    404 page and plugin

---
+++ src/pages/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <Layout>
+    <!-- prettier-ignore -->
+    <div class="w-full flex flex-col justify-center items-center bg-background-base mt-32">
+    <pre class="text-accent-tag">
+    :::     :::::::     :::     
+   :+:     :+:   :+:   :+:      
+  +:+ +:+  +:+  :+:+  +:+ +:+   
+ +#+  +:+  +#+ + +:+ +#+  +:+   
++#+#+#+#+#++#+#  +#++#+#+#+#+#+ 
+      #+#  #+#   #+#      #+#   
+      ###   #######       ###    
+    </pre>
+    <pre class="text-accent-key">
+# #     #       #    #       #       # ########        #   
+ #   #  #       #   ######## #       #        # ########## 
+# # #          #   #       #        #        #        ##   
+   #          #   #      ##        #       ##       ## #   
+  # #       ##         ##        ##      ## #     ##   #   
+ #   #    ##         ##        ##       ##   #  ##    ##   
+      # ##         ##        ##        #      #        #   
+    </pre>
+  </div>
+    <div class="text-center font-dankit">There's nothing here bruh</div>
+  </Layout>
+</template>
